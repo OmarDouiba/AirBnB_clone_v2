6395fdb2cd19859f91318e89b7a99fbf0c3abe1c
DouibaOmar <douibaomar40@gmail.com>
2024-02-17 13:39:54 +0100

    Done

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    data = storage.all("State")
+    return render_template('7-states_list', data=data)
