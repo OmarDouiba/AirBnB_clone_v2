11c43bc9544e4ac771f8686d1128cc3ccb6ba2f3
DouibaOmar <douibaomar40@gmail.com>
2024-02-18 13:41:08 +0100

    Done

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
@@ -11,10 +12,11 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = "amenities"
-    name = Column(
-        String(128),
-        nullable=False)
-    place_amenities = relationship(
-        "Place",
-        secondary=place_amenity)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place",
+            secondary=place_amenity, back_populates="amenities")
+    else:
+        name = ""
