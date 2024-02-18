b2b478e7f6e075cd3ac8a4075f4e2bf76112402d
DouibaOmar <douibaomar40@gmail.com>
2024-02-18 13:52:43 +0100

    Done

--- models/review.py
+++ models/review.py
@@ -2,6 +2,7 @@
 """This is the review class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
 class Review(BaseModel, Base):
@@ -11,15 +12,12 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
-    __tablename__ = "reviews"
-    text = Column(
-        String(1024),
-        nullable=False)
-    place_id = Column(
-        String(60),
-        ForeignKey("places.id"),
-        nullable=False)
-    user_id = Column(
-        String(60),
-        ForeignKey("users.id"),
-        nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
