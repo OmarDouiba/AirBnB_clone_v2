8359547eaa198835f254324f399d584ca3ed2d4b
DouibaOmar <douibaomar40@gmail.com>
2024-02-18 16:52:36 +0100

    Done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,46 @@ from models import storage
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<name>', strict_slashes=False)
+def c_text(name):
+    """Displays 'C' followed by the value of the text variable"""
+    return "C {}".format(name.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Displays 'Python' followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Displays 'n is a number' only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_file(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with the states listed in alphabetical order"""
