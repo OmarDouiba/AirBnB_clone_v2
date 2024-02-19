ae1b9ad2dac44b0e4af0648eb2d276db93d4b5fd
DouibaOmar <douibaomar40@gmail.com>
2024-02-19 08:58:54 +0100

    Done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""
+Starts a Flask web application
+"""
 from flask import Flask, render_template
 from models import storage
 from models import *
@@ -10,14 +12,18 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with render template"""
+    """
+    Displays an HTML page with render template
+    """
     States = storage.all(State)
     return render_template('7-states_list.html', states=States)
 
 
 @app.teardown_appcontext
 def tear_db(exception):
-    """Closes the storage"""
+    """
+    Closes the storage
+    """
     storage.close()
 
 
